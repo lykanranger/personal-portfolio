2c74134ab44db6b5619b73fd02cc38461526c82f
Karun Binny <95923091+lykanranger@users.noreply.github.com>
2025-07-06 12:05:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+
+// TOGGLEMENU 
+
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+
+// TYPEING
+
+const typingText = document.getElementById("typing-text");
+const text = "KARUN BINNY";
+let index = 0;
+
+function typeText() {
+  if (index < text.length) {
+    typingText.innerHTML += text[index];
+    index++;
+    setTimeout(typeText, 150);
+  } else {
+    setTimeout(() => {
+      typingText.innerHTML = "";
+      index = 0;
+      typeText();
+    }, 1000);
+  }
+}
+
+typeText();
+
+// LOADING batman
+
+document.addEventListener('DOMContentLoaded', function() {
+  const loadingButton = document.querySelector('.loading-button');
+  const loadingContainer = document.querySelector('.loading-container');
+  
+  setTimeout(() => {
+    loadingButton.classList.add('show');
+  }, 100);
+  
+  setTimeout(() => {
+    loadingContainer.style.display = 'none';
+    // window.location.href = 'https://your-website-url.com';
+  }, 3000); 
+});
+
+// CONTACT
+
+const form = document.querySelector('.app-form');
+
+form.addEventListener('submit', (e) => {
+  const name = form.querySelector('input[name="name"]').value;
+  const email = form.querySelector('input[name="email"]').value;
+  const contact = form.querySelector('input[name="contact"]').value;
+  const message = form.querySelector('textarea[name="message"]').value;
+
+  if (!name || !email || !contact || !message) {
+    alert('Please fill out all fields');
+    e.preventDefault();
+  }
+});
